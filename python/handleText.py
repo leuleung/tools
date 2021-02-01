f7bd2140ae71664a85c4a7c07d217c23d2a67b20
liang.huajun <ltu.hjl@gmail.com>
2021-02-01 16:56:46 +0800

    1. 初版测试可行. data.txt为测试数据.

--- python/handleText.py
+++ python/handleText.py
@@ -2,28 +2,44 @@
 # -*- coding: UTF-8 -*-
 
 
-
 def readConfiguration(path):
     """通过json配置文件, 获得配置信息.
 
     """
+    info = []
+    channle = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
     f = open(path, 'r')
-    data = f.readline()
-    print(data)
-    
+    data = f.readlines()
+    for i in range(len(data)):
+        if len(data[i].split()) == 64:
+            info.append(data[i].split())
+            # print(data[i].split())
+            pass
+        else:
+            # print(data[i])
+            pass
 
+    for lines in range(len(info)):
+        channle_index = 0  # 16个通道序列号
+        for i in range(0, 63, 4):
+            # print("Hello is", info[lines][0])
+            for index in range(4):
+                # print(info[lines][i+index])
+                channle[channle_index].append(info[lines][i+index])
+            channle_index = channle_index + 1
+        pass
 
-# def _save_tsi2tempfile_(filename, data):
-#     """后面程序的数据交换通过这个文件
-#     """
-#     with open(filename, 'w') as f:
-#         # print('Type:', type(data))
-#         f.write(str(data))
+    channle_str = str(channle).replace("\'", "")
+    # print(channle_str)
+    channle_str = channle_str.replace("[", "")
+    channle_str = channle_str.replace("], ", "\n")
+    fp = open('hello.csv', 'w')
+    fp.writelines(str(channle_str))
 
 
 def main():
-    readConfiguration('')
+    readConfiguration('./data.txt')
+
 
 if __name__ == "__main__":
     main()
-
