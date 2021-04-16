00ef92ed726b457e63ed269c2d8674990f54726a
liang.huajun <ltu.hjl@gmail.com>
2021-04-16 17:44:21 +0800

    Add saving to csv function.

--- python/drawTouch/save_Touch2csv.py
+++ python/drawTouch/save_Touch2csv.py
@@ -10,7 +10,11 @@ import struct
 
 gjson = {
     "scanTimes": 11,
-    "nameCSV": "TSI_Fine_All_channle.csv",
+    "nameCSV_fine": "fine.csv",
+    "nameCSV_average": "average.csv",
+    "nameCSV_variance": "variance.csv",
+    "nameCSV_baseline": "baseline.csv",
+    "nameCSV_touch": "touch.csv",
     "nameTempfile": "temp",
     "baudrate": 115200,
     "port": "COM0"
@@ -25,11 +29,14 @@ def readConfiguration(jsonName='TouchConfiguration.json'):
     f = open(jsonName, 'r')
     data = json.load(f)
     gjson['scanTimes'] = data['scanTimes']
-    gjson['nameCSV'] = data['nameCSV']
-    gjson['nameTempfile'] = data['nameTempfile']
-    gjson['baudrate'] = data['save_TSIFine_csv.py']['baudrate']
-    gjson['port'] = data['save_TSIFine_csv.py']['port']
-    print(gjson)
+    gjson['baudrate'] = data['save_Touch2csv.py']['baudrate']
+    gjson['port'] = data['save_Touch2csv.py']['port']
+    gjson['nameCSV_fine'] = data['nameCSV_fine']
+    gjson['nameCSV_average'] = data['nameCSV_average']
+    gjson['nameCSV_variance'] = data['nameCSV_variance']
+    gjson['nameCSV_baseline'] = data['nameCSV_baseline']
+    gjson['nameCSV_touch'] = data['nameCSV_touch']
+    # print(gjson)
 
 
 def _save_tsi2tempfile_(filename, data):
@@ -40,7 +47,7 @@ def _save_tsi2tempfile_(filename, data):
         f.write(str(data))
 
 
-def _save_tsi2csv_(csv_name, data):
+def _save_fine2csv_(csv_name, data):
     """
     """
     with open(csv_name, 'a') as f:
@@ -52,18 +59,17 @@ def _save_tsi2csv_(csv_name, data):
             f_csv.writerow(temp_list)
 
 
+def _save_singleData2csv_(csv_name, data):
+    """
+    """
+    with open(csv_name, 'a') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(data)
+
+
 def save_data2csv():
     """
     """
-
-    strs = ''
-
-    '''init csv header'''
-    headers = ['ch0', 'ch1', 'ch2', 'ch3', 'ch4', 'ch5', 'ch6', 'ch7',
-               'ch8', 'ch9', 'ch10', 'ch11', 'ch12', 'ch13', 'ch14', 'ch15']
-    with open(gjson['nameCSV'], 'w') as f:
-        f_csv = csv.writer(f)
-        f_csv.writerow(headers)
 
     '''init serial'''
     ser = serial.Serial()
@@ -102,25 +108,25 @@ def save_data2csv():
 
             # got average data
             for i in range(16):
-                data_averege.append(struct.unpack('>f', a[1436+i*4:1440+i*4])[0])
+                data_averege.append(struct.unpack('<f', a[1436+i*4:1440+i*4])[0])
             # print('Average:', data_averege)
 
             # got Variance data
             for i in range(16):
-                data_variance.append(struct.unpack('>f', a[1500+i*4:1504+i*4])[0])
+                data_variance.append(struct.unpack('<f', a[1500+i*4:1504+i*4])[0])
             # print('Variance:', data_variance)
 
             # got Baseline data
             for i in range(16):
                 index_baseline = struct.unpack('>B', a[1584:1585])[0]
                 if index_baseline == 0:
-                    data_baseline.append(struct.unpack('>f', a[1588+i*4:1592+i*4])[0])
+                    data_baseline.append(struct.unpack('<f', a[1588+i*4:1592+i*4])[0])
                     pass
                 elif index_baseline == 1:
-                    data_baseline.append(struct.unpack('>f', a[1652+i*4:1656+i*4])[0])
+                    data_baseline.append(struct.unpack('<f', a[1652+i*4:1656+i*4])[0])
                     pass
                 elif index_baseline == 2:
-                    data_baseline.append(struct.unpack('>f', a[1716+i*4:1720+i*4])[0])
+                    data_baseline.append(struct.unpack('<f', a[1716+i*4:1720+i*4])[0])
                 else:
                     print('index baseline is wrong...')
                     break
@@ -128,60 +134,56 @@ def save_data2csv():
             # print('Baseline:', data_baseline)
 
             # got Touch valid
-            Touch_All = struct.unpack('>H', a[1566:1568])[0]
+            Touch_All = struct.unpack('<H', a[1566:1568])[0]
             for i in range(16):
                 if (Touch_All & (2^i)) != 0:
                     data_touch.append(1)
                 else:
                     data_touch.append(0)
-            print('Touch:', data_touch)
+            # print('Touch:', data_touch)
 
         else:
             print("data is wrong")
             break
 
-        # if strs[-1] == '}':
-        #     try:
-        #         thisJson = json.loads(strs)
-        #     except json.decoder.JSONDecodeError:
-        #         # 数据传输错误, 有丢包
-        #         print('JSON data having Error...')
-        #     else:
-        #         if thisJson['MessageType'] == 'Log':
-        #             print(thisJson['Log'])
-        #         elif thisJson['MessageType'] == 'Data':
-
-        #             try:
-        #                 '''got data from JSON'''
-        #                 data = thisJson['Data']  # data is a int-list
-        #             except KeyError:
-        #                 print('JSON decode having Error, need retransfer...')
-        #             else:
-        #                 '''1. 参数校验：CRC16'''
-        #                 try:
-        #                     if thisJson['Parameter'].find("CRC16") >= 0:
-        #                         crc_check = Crc16CcittFalse.calc(data)
-        #                         if crc_check == ((thisJson['CRC16'][1]*int(0x100)) + thisJson['CRC16'][0]):
-        #                             '''save to csv file'''
-        #                             _save_tsi2tempfile_(
-        #                                 gjson['nameTempfile'], data)
-        #                             _save_tsi2csv_(
-        #                                 gjson['nameCSV'], data)
-        #                         else:
-        #                             print('CRC not Equal, need retransfer...')
-        #                     else:
-        #                         '''save to csv file'''
-        #                         _save_tsi2tempfile_(
-        #                             gjson['nameTempfile'], data)
-        #                         _save_tsi2csv_(gjson['nameCSV'], data)
-        #                 except KeyError:
-        #                     print('JSON decode having Error, need retransfer...')
-        #     strs = ''
+        # save data to csv
+        _save_fine2csv_(gjson['nameCSV_fine'], data_fine)
+        _save_singleData2csv_(gjson['nameCSV_average'], data_averege)
+        _save_singleData2csv_(gjson['nameCSV_variance'], data_variance)
+        _save_singleData2csv_(gjson['nameCSV_baseline'], data_baseline)
+        _save_singleData2csv_(gjson['nameCSV_touch'], data_touch)
+        
     ser.close()
 
 
 def main():
+
     readConfiguration()
+
+    '''init csv header'''
+    headers = ['ch0', 'ch1', 'ch2', 'ch3', 'ch4', 'ch5', 'ch6', 'ch7',
+               'ch8', 'ch9', 'ch10', 'ch11', 'ch12', 'ch13', 'ch14', 'ch15']
+    data_filling = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    with open(gjson['nameCSV_fine'], 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(headers)
+    with open(gjson['nameCSV_average'], 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(headers)
+        f_csv.writerow(data_filling)
+    with open(gjson['nameCSV_variance'], 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(headers)
+        f_csv.writerow(data_filling)
+    with open(gjson['nameCSV_baseline'], 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(headers)
+        f_csv.writerow(data_filling)
+    with open(gjson['nameCSV_touch'], 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(headers)
+        f_csv.writerow(data_filling)
+
     save_data2csv()
 
 
